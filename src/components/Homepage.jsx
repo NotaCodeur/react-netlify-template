edfbf301db6f87846595f3f471a872fe1eb6d948
nemo <appsbynemo@gmail.com>
2022-05-03 13:27:44 +0200

    removed crypto and news

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -516,24 +516,24 @@ const Homepage = () => {
       </div>
       <br />
       <br />
-      <Title level={2} className='heading'>Global Crypto Stats</Title>
+      {/* <Title level={2} className='heading'>Global Crypto Stats</Title>
       <Row>
         <Col span={12}><Statistic title='Total Cryptocurrencies' value={globalStats.total} /></Col>
         <Col span={12}><Statistic title='Total Exchanges' value={millify(globalStats.totalExchanges)} /></Col>
         <Col span={12}><Statistic title='Total Marketcap' value={millify(globalStats.totalMarketCap)} /></Col>
         <Col span={12}><Statistic title='Total 24h Volume' value={millify(globalStats.total24hVolume)} /></Col>
         <Col span={12}><Statistic title='Total Markets' value={millify(globalStats.totalMarkets)} /></Col>
-      </Row>
-      <div className='home-heading-container'>
+      </Row> */}
+      {/* <div className='home-heading-container'>
         <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the World</Title>
         <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
-      </div>
-      <Cryptocurrencies simplified />
-      <div className='home-heading-container'>
+      </div> */}
+      {/* <Cryptocurrencies simplified /> */}
+      {/* <div className='home-heading-container'>
         <Title level={2} className='home-title'>Latest Crypto News</Title>
         <Title level={3} className='show-more'><Link to='/news'>Show More</Link></Title>
-      </div>
-      <News simplified />
+      </div> */}
+      {/* <News simplified /> */}
     </>
   )
 }
