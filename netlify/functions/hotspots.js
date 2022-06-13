4cfb7dfd43eaf64449e9ad25d19c543e753ff8c7
nemo <appsbynemo@gmail.com>
2022-06-13 14:09:55 +0200

    ; -> ,

--- netlify/functions/hotspots.js
+++ netlify/functions/hotspots.js
@@ -23,7 +23,7 @@ exports.handler = async function(event, context) {
     } catch(error) {
         return {
             statusCode: 500,
-            body: JSON.stringify(error);
+            body: JSON.stringify(error),
         }
     }
 
