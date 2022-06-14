0b91b26f0cd1e398728149955d5a31689384eaf3
nemo <appsbynemo@gmail.com>
2022-06-14 20:51:33 +0200

    log body of fetch for each hotspot in homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -139,8 +139,8 @@ const Homepage = () => {
     accountObj.hotspots.forEach(async Element => {
       console.log(Element)
       const monthEarn = await fetch(`/.netlify/functions/hotspotsMonth?search=/v1/hotspots/${Element.address}/rewards/sum`)
+      const dataaa = await monthEarn.body.json
       console.log(monthEarn)
-      const dataaa = await monthEarn.json
       console.log(dataaa)
       if(dataaa.length){
       }
